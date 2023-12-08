afa96744cf99417ef7b566c71ba5bc278761ff12
David Belleau <david-belleau@hotmail.com>
2023-12-08 23:40:59 +0100

    feat/create component

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+// app/layout.tsx
+import { Footer } from '@/components/layout/Footer';
+import { Header } from '@/components/layout/Header';
+import { TailwindIndicator } from '@/components/utils/TailwindIndicator';
+import { SiteConfig } from '@/lib/site-config';
+import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { PropsWithChildren } from 'react';
+import { Providers } from './Providers';
+import './globals.css';
+
+const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+export const metadata: Metadata = {
+  title: SiteConfig.title,
+  description: SiteConfig.description,
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <html lang="en" className="h-full" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            'h-full bg-background font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          <Providers>
+            <div className="relative flex min-h-screen flex-col">
+              <Header />
+              <div className="flex-1">{children}</div>
+              <Footer />
+            </div>
+            <TailwindIndicator />
+          </Providers>
+        </body>
+      </html>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <div>
+      <Card >
+        <CardHeader>
+          <CardTitle>Hello world</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+}
