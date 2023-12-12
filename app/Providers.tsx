a7b66784b5eaca4d32196885e3cc6a4e9d54a686
David Belleau <david-belleau@hotmail.com>
2023-12-12 16:40:16 +0100

    feat/import QueryClientProvider and ReactQueryDevtools

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,19 +1,26 @@
-// app/Providers.tsx
-'use client';
+"use client";
 
-import { SessionProvider } from 'next-auth/react';
-import { ThemeProvider } from 'next-themes';
-import { PropsWithChildren } from 'react';
-import { Toaster } from 'sonner';
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Toaster />
-        <SessionProvider>{children}</SessionProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <SessionProvider>
+            <Toaster />
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </SessionProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 };
-
