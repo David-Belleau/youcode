e0fbc8d50f70d9c90faf5aed5ff489bc1244903d
David Belleau <david-belleau@hotmail.com>
2023-12-12 16:43:05 +0100

    feat/modify export

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -12,6 +12,4 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export prisma;
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
