9031b1de3b3503cdb9ded1d7f8b36dc105af515a
David Belleau <david-belleau@hotmail.com>
2023-12-17 20:45:44 +0100

    feat/create component

---
+++ app/admin/page.tsx
@@ -0,0 +1,21 @@
+import {
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from '@/components/layout/layout';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default async function CoursesPage() {
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent>
+        <Link href="/admin/courses" className={buttonVariants({variant: "outline"} )}>My courses</Link>
+      </LayoutContent>
+    </Layout>
+  );
+}
