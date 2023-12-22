9c5e3403af2c1ef898a2c51a088da400fcd34114
David Belleau <david-belleau@hotmail.com>
2023-12-22 19:44:49 +0100

    feat/create function

---
+++ app/courses/course.query.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+export const getCourses = async (userId?: string) => {
+  return await prisma.course.findMany({
+    // get at least one course from the course creator
+    where: userId
+      ? {
+          users: {
+            some: {
+              userId,
+            },
+          },
+        }
+      : undefined,
+    select: {
+      name: true,
+      image: true,
+      presentation: true,
+      id: true,
+      creator: {
+        select: {
+          image: true,
+          name: true,
+        },
+      },
+    },
+  });
+};
+
+// get course model types from prisma
+export type CoursesCard = Prisma.PromiseReturnType<typeof getCourses>[number];
