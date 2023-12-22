5ffaa8993bf563703d1455616e59651e0c483d74
David Belleau <david-belleau@hotmail.com>
2023-12-22 19:46:07 +0100

    feat/add link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,30 @@
-import { SiteConfig } from '@/lib/site-config';
-import Link from 'next/link';
-import { ThemeToggle } from '@/components/theme/ThemeToggle';
-import { Typography } from '@/components/ui/typography';
-import Image from 'next/image';
-import { AuthButton } from '../../features/auth/AuthButton';
+import { SiteConfig } from "@/lib/site-config";
+import Link from "next/link";
+import { ThemeToggle } from "@/components/theme/ThemeToggle";
+import { Typography } from "@/components/ui/typography";
+import Image from "next/image";
+import { AuthButton } from "../../features/auth/AuthButton";
+import { buttonVariants } from "../ui/button";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex gap-6 md:gap-10">
-        <Image src="/images/logo.svg" width={50} height={35} alt="app logo" />
-          <Typography variant="h3" as={Link} href="/">
-            {SiteConfig.title}
-          </Typography>
+        <div className="flex items-center gap-2">
+          <Image src="/images/logo.svg" width={50} height={35} alt="app logo" />
+          <div className="flex items-baseline gap-2">
+            <Typography variant="h3" as={Link} href="/">
+              {SiteConfig.title}
+            </Typography>
+            <Typography
+              as={Link}
+              variant="link"
+              href="/explorer"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              Explorer
+            </Typography>
+          </div>
         </div>
 
         <div className="flex flex-1 items-center justify-end space-x-4">
