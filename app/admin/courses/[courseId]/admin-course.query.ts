a1a15a3cfe1a6e91acd193cc974304182d1b9c17
David Belleau <david-belleau@hotmail.com>
2023-12-26 10:53:43 +0100

    feat/modify file name

---
+++ app/admin/courses/[courseId]/admin-course.query.ts
@@ -0,0 +1,57 @@
+import { prisma } from '@/lib/prisma';
+
+export const getAdminCourse = async ({
+  courseId,
+  userId,
+  userPage,
+}: {
+  courseId: string;
+  userId: string;
+  userPage: number;
+}) => {
+  const course = await prisma.course.findUnique({
+    where: {
+      creatorId: userId,
+      id: courseId,
+    },
+    select: {
+      id: true,
+      image: true,
+      name: true,
+      presentation: true,
+      users: {
+        take: 5,
+        skip: Math.max(0, userPage * 5),
+        select: {
+          canceledAt: true,
+          id: true,
+          user: {
+            select: {
+              email: true,
+              id: true,
+              image: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          lessons: true,
+          users: true,
+        },
+      },
+    },
+  });
+
+  const users = course?.users.map((user) => {
+    return {
+      canceled: user.canceledAt ? true : false,
+      ...user.user,
+    };
+  });
+
+  return {
+    ...course,
+    users,
+  };
+};
