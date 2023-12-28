e53574d8995e14208a058ece0caebfcec1a7a75b
David Belleau <david-belleau@hotmail.com>
2023-12-28 16:30:00 +0100

    feat/create custom hook

---
+++ src/components/hooks/useIsClient.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+// only client side redndering
+export const useIsClient = (): boolean => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
