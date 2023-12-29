ff666857cfa82f6e3f559f08334f50971ed4ceb5
David Belleau <david-belleau@hotmail.com>
2023-12-29 19:48:47 +0100

    feat/add link

--- app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
+++ app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -1,45 +1,59 @@
 import {
-    Layout,
-    LayoutContent,
-    LayoutHeader,
-    LayoutTitle,
-  } from '@/components/layout/layout';
-  import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-  import { getRequiredAuthSession } from '@/lib/auth';
-  import { notFound } from 'next/navigation';
-  import { LessonDetail } from './LessonDetailsForm';
-  import { getAdminLesson } from './lesson.query';
-  
-  export default async function CourseLessonsPage({
-    params,
-  }: {
-    params: {
-      lessonId: string;
-    };
-  }) {
-    const session = await getRequiredAuthSession();
-  
-    const lesson = await getAdminLesson(params.lessonId, session.user.id);
-  
-    if (!lesson) {
-      notFound();
-    }
-  
-    return (
-      <Layout>
-        <LayoutHeader>
-          <LayoutTitle>{lesson.name}</LayoutTitle>
-        </LayoutHeader>
-        <LayoutContent className="flex flex-col gap-4 lg:flex-row">
-          <Card className="flex-[2]">
-            <CardHeader>
-              <CardTitle>Details</CardTitle>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-              <LessonDetail defaultValue={lesson} />
-            </CardContent>
-          </Card>
-        </LayoutContent>
-      </Layout>
-    );
+  Layout,
+  LayoutActions,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/layout";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getRequiredAuthSession } from "@/lib/auth";
+import { notFound } from "next/navigation";
+import { LessonDetail } from "./form/LessonDetailsForm";
+import { getAdminLesson } from "./lesson.query";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function CourseLessonsPage({
+  params,
+}: {
+  params: {
+    lessonId: string;
+  };
+}) {
+  const session = await getRequiredAuthSession();
+
+  const lesson = await getAdminLesson(params.lessonId, session.user.id);
+
+  if (!lesson) {
+    notFound();
   }
+
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>{lesson.name}</LayoutTitle>
+      </LayoutHeader>
+      <LayoutActions>
+        <Link
+          className={buttonVariants({
+            size: "sm",
+            variant: "secondary",
+          })}
+          href={`/admin/courses/${lesson.courseId}/lessons`}
+        >
+          Back
+        </Link>
+      </LayoutActions>
+      <LayoutContent className="flex flex-col gap-4 lg:flex-row">
+        <Card className="flex-[2]">
+          <CardHeader>
+            <CardTitle>Details</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <LessonDetail defaultValue={lesson} />
+          </CardContent>
+        </Card>
+      </LayoutContent>
+    </Layout>
+  );
+}
