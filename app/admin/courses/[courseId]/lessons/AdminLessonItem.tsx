899b40588dcb23244108d6b3d9b4789b6ffff163
David Belleau <david-belleau@hotmail.com>
2023-12-29 17:47:13 +0100

    feat/add link

--- app/admin/courses/[courseId]/lessons/AdminLessonItem.tsx
+++ app/admin/courses/[courseId]/lessons/AdminLessonItem.tsx
@@ -1,16 +1,19 @@
 import { Badge } from "@/components/ui/badge";
 import { Typography } from "@/components/ui/typography";
 import { AdminLessonItemType } from "./lessons.query";
+import Link from "next/link";
 
 export type AdminLessonItemProps = {
   lesson: AdminLessonItemType;
 };
 
-export const AdminLessonItem = ({lesson}: AdminLessonItemProps) => {
+export const AdminLessonItem = ({ lesson }: AdminLessonItemProps) => {
   return (
-    <div className="flex items-center rounded border border-border bg-card px-4 py-2 transition-colors hover:bg-accent">
-      <Typography variant="large">{lesson.name}</Typography>
-      <Badge className="ml-auto">{lesson.state}</Badge>
-    </div>
+    <Link href={`/admin/courses/${lesson.courseId}/lessons/${lesson.id}`}>
+      <div className="flex items-center rounded border border-border bg-card px-4 py-2 transition-colors hover:bg-accent">
+        <Typography variant="large">{lesson.name}</Typography>
+        <Badge className="ml-auto">{lesson.state}</Badge>
+      </div>
+    </Link>
   );
 };
