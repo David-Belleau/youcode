34743f842ba207c35ed0234b459a5fc7ce1df4b5
David Belleau <david-belleau@hotmail.com>
2024-01-02 20:10:31 +0100

    feat/add card component

--- app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
+++ app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -12,6 +12,8 @@ import { LessonDetail } from "./form/LessonDetailsForm";
 import { getAdminLesson } from "./lesson.query";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { MDXEditor, headingsPlugin } from "@mdxeditor/editor";
+import { MdxEditor } from "./content/MdxEditor"
 
 export default async function CourseLessonsPage({
   params,
@@ -29,7 +31,7 @@ export default async function CourseLessonsPage({
   }
 
   return (
-    <Layout>
+    <Layout className="max-w-5xl">
       <LayoutHeader>
         <LayoutTitle>{lesson.name}</LayoutTitle>
       </LayoutHeader>
@@ -45,12 +47,20 @@ export default async function CourseLessonsPage({
         </Link>
       </LayoutActions>
       <LayoutContent className="flex flex-col gap-4 lg:flex-row">
-        <Card className="flex-[2]">
+        <Card className="flex-1">
           <CardHeader>
             <CardTitle>Details</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
             <LessonDetail defaultValue={lesson} />
+          </CardContent>
+        </Card>
+        <Card className="flex-[3]">
+          <CardHeader>
+            <CardTitle>Content</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <MdxEditor lessonId={lesson.id} markdown={lesson.content} />
           </CardContent>
         </Card>
       </LayoutContent>
