68f5fc82debc596aad25490c7d79230011ecb3c8
David Belleau <david-belleau@hotmail.com>
2024-01-10 17:18:43 +0100

    feat/add modal prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
-import { Footer } from '@/components/layout/Footer';
-import { Header } from '@/components/layout/Header';
-import { TailwindIndicator } from '@/components/utils/TailwindIndicator';
-import { SiteConfig } from '@/lib/site-config';
-import { cn } from '@/lib/utils';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { PropsWithChildren } from 'react';
-import { Providers } from './Providers';
-import './globals.css';
+import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/layout/Header";
+import { TailwindIndicator } from "@/components/utils/TailwindIndicator";
+import { SiteConfig } from "@/lib/site-config";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { PropsWithChildren, ReactNode } from "react";
+import { Providers } from "./Providers";
+import "./globals.css";
 
-const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: SiteConfig.title,
   description: SiteConfig.description,
 };
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default function RootLayout({
+  children,
+  modal,
+}: PropsWithChildren<{
+  modal?: ReactNode;
+}>) {
   return (
     <>
       <html lang="en" className="h-full" suppressHydrationWarning>
         <head />
         <body
           className={cn(
-            'h-full bg-background font-sans antialiased',
+            "h-full bg-background font-sans antialiased",
             fontSans.variable
           )}
         >
@@ -34,6 +39,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
               <Footer />
             </div>
             <TailwindIndicator />
+            {modal}
           </Providers>
         </body>
       </html>
