ee26ed91997c369f688dc26a67f8d9f9ac74bbbe
David Belleau <david-belleau@hotmail.com>
2024-01-19 18:00:43 +0100

    feat/add a parameter and components + create a nullish coalescing operator

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -8,25 +8,53 @@ import { NotAuthenticatedCard } from "@/features/error/NotAuthenticatedCard";
 import { getRequiredAuthSession } from "@/lib/auth";
 import { CourseCard } from "../courses/CourseCard";
 import { getCourses } from "../courses/course.query";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
+import { CoursePaginationButton } from "@/features/pagination/CoursePaginationButton";
 
-export default async function CourseOnUserPage() {
+export default async function CoursesPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const session = await getRequiredAuthSession();
 
   if (!session?.user.id) {
     return <NotAuthenticatedCard />;
   }
 
-  const courses = await getCourses(session.user.id);
+  const page = Number(searchParams.page ?? 0) ?? 0;
+  const { courses, totalCourses } = await getCourses({
+    userId: session.user.id,
+    page,
+  });
 
   return (
     <Layout>
       <LayoutHeader>
         <LayoutTitle>Your courses</LayoutTitle>
       </LayoutHeader>
-      <LayoutContent className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
-        {courses.map((course) => (
-          <CourseCard course={course} key={course.id} />
-        ))}
+      <LayoutContent className="flex flex-col gap-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
+          {courses.map((course) => (
+            <CourseCard course={course} key={course.id} />
+          ))}
+        </div>
+        {courses.length === 0 ? (
+          <Alert>
+            <AlertTriangle />
+            <AlertTitle>You are not enrolled in any courses yet.</AlertTitle>
+            <AlertDescription>
+              Please go to the explorer page to find a course.
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <CoursePaginationButton
+            baseUrl={`/courses`}
+            page={page}
+            totalPage={totalCourses}
+          />
+        )}
       </LayoutContent>
     </Layout>
   );
--- app/explorer/page.tsx
+++ app/explorer/page.tsx
@@ -6,19 +6,46 @@ import {
 } from '@/components/layout/layout';
 import { CourseCard } from '../courses/CourseCard';
 import { getCourses } from '../courses/course.query';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertTriangle } from 'lucide-react';
+import { CoursePaginationButton } from '@/features/pagination/CoursePaginationButton';
 
-export default async function ExplorerPage() {
-  const courses = await getCourses();
+export default async function ExplorerPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const page = Number(searchParams.page ?? 0) ?? 0;
+  const { courses, totalCourses } = await getCourses({ page });
 
   return (
     <Layout>
       <LayoutHeader>
         <LayoutTitle>Explorer</LayoutTitle>
       </LayoutHeader>
-      <LayoutContent className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
-        {courses.map((course) => (
-          <CourseCard course={course} key={course.id} />
-        ))}
+      <LayoutContent className="flex flex-col gap-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
+          {courses.map((course) => (
+            <CourseCard course={course} key={course.id} />
+          ))}
+        </div>
+        {courses.length === 0 ? (
+          <Alert>
+            <AlertTriangle />
+            <AlertTitle>
+              There are no courses yet. Please come back later.
+            </AlertTitle>
+            <AlertDescription>
+              We are working hard to bring you the best courses.
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <CoursePaginationButton
+            baseUrl={`/courses`}
+            page={page}
+            totalPage={totalCourses}
+          />
+        )}
       </LayoutContent>
     </Layout>
   );
