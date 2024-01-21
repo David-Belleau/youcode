000c5b0def8cc7597aae7cbc69122fd978fcf0c3
David Belleau <david-belleau@hotmail.com>
2024-01-21 19:26:45 +0100

    feat/add condition "if"

--- app/admin/courses/new/page.tsx
+++ app/admin/courses/new/page.tsx
@@ -1,28 +1,32 @@
 import {
-    Layout,
-    LayoutContent,
-    LayoutHeader,
-    LayoutTitle,
-  } from '@/components/layout/layout';
-  import { Card, CardContent } from '@/components/ui/card';
-  import { getRequiredAuthSession } from '@/lib/auth';
-  import { CourseForm } from '../[courseId]/edit/CourseForm';
+  Layout,
+  LayoutContent,
+  LayoutHeader,
+  LayoutTitle,
+} from "@/components/layout/layout";
+import { Card, CardContent } from "@/components/ui/card";
+import { getRequiredAuthSession } from "@/lib/auth";
+import { CourseForm } from "../[courseId]/edit/CourseForm";
+import { NotAuthenticatedCard } from "@/features/error/NotAuthenticatedCard";
+
+export default async function CoursePage() {
+  const session = await getRequiredAuthSession();
   
-  export default async function CoursePage() {
-    await getRequiredAuthSession();
-  
-    return (
-      <Layout>
-        <LayoutHeader>
-          <LayoutTitle>Create course</LayoutTitle>
-        </LayoutHeader>
-        <LayoutContent>
-          <Card className="flex-[2]">
-            <CardContent className="mt-6">
-              <CourseForm />
-            </CardContent>
-          </Card>
-        </LayoutContent>
-      </Layout>
-    );
+  if (!session?.user.id) {
+    return <NotAuthenticatedCard />;
   }
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Create course</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent>
+        <Card className="flex-[2]">
+          <CardContent className="mt-6">
+            <CourseForm />
+          </CardContent>
+        </Card>
+      </LayoutContent>
+    </Layout>
+  );
+}
