4aa5768c8b04ccdf200c0b9ba7ab310c42464918
David Belleau <david-belleau@hotmail.com>
2024-01-21 19:31:02 +0100

    feat/add items in DropDownMenu component

--- src/features/auth/LoggedInButton.tsx
+++ src/features/auth/LoggedInButton.tsx
@@ -1,16 +1,6 @@
 "use client";
 
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -20,79 +10,73 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Loader } from "@/components/ui/loader";
 import { useMutation } from "@tanstack/react-query";
-import { LogOut, User2 } from "lucide-react";
+import { GraduationCap, LogOut, User2, Map } from "lucide-react";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export type LoggedInButtonProps = {
   user: Session["user"];
 };
 
-export const LoggedInButton = ({user}: LoggedInButtonProps) => {
+export const LoggedInButton = ({ user }: LoggedInButtonProps) => {
   const mutation = useMutation({
     mutationFn: async () => {
       signOut();
+      redirect("/");
     },
   });
 
   return (
     <DropdownMenu>
-      <AlertDialog>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="sm">
-            <Avatar className="mr-2 h-6 w-6">
-              <AvatarFallback>{user?.name?.[0]}</AvatarFallback>
-              {user.image && (
-                <AvatarImage
-                  src={user.image}
-                  alt={user.name ?? "user picture"}
-                />
-              )}
-            </Avatar>
-            {user.name}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem asChild>
-            <Link href="/account">
-              <User2 className="mr-2" size={12} />
-              My account
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-            <DropdownMenuItem>
+      <DropdownMenuTrigger>
+        <Avatar className="mr-2 h-6 w-6 cursor-pointer">
+          <AvatarFallback>{user?.name?.[0]}</AvatarFallback>
+          {user.image && (
+            <AvatarImage src={user.image} alt={user.name ?? "user picture"} />
+          )}
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem asChild>
+          <Link href="/account">
+            <User2 className="mr-2" size={12} />
+            My account
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="block sm:hidden" />
+        <DropdownMenuItem asChild className="block sm:hidden">
+          <Link href="/explorer" className="flex flex-row">
+            <Map className="mr-2" size={12} />
+            Explorer
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="block sm:hidden" />
+        <DropdownMenuItem asChild className="block sm:hidden">
+          <Link href="/courses" className="flex flex-row">
+            <GraduationCap className="mr-2" size={12} />
+            Courses
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <DropdownMenuItem
+            className="border-none"
+            disabled={mutation.isPending}
+            onClick={() => {
+              mutation.mutate();
+            }}
+          >
+            {mutation.isPending ? (
+              <Loader className="mr-2" size={12} />
+            ) : (
               <LogOut className="mr-2" size={12} />
-              Logout
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              Are you sure you want to logout?
-            </AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel asChild>
-              <Button variant="secondary">Cancel</Button>
-            </AlertDialogCancel>
-            <Button
-              variant="destructive"
-              disabled={mutation.isPending}
-              onClick={() => {
-                mutation.mutate();
-              }}
-            >
-              {mutation.isPending ? (
-                <Loader className="mr-2" size={12} />
-              ) : (
-                <LogOut className="mr-2" size={12} />
-              )}
-              Logout
-            </Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+            )}
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
