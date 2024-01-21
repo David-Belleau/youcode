0b6d42e8262cb89d0585f49da16fa767dbc4b4af
David Belleau <david-belleau@hotmail.com>
2024-01-21 19:31:31 +0100

    feat/add CardFooter component

--- src/features/error/NotAuthenticatedCard.tsx
+++ src/features/error/NotAuthenticatedCard.tsx
@@ -1,11 +1,15 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { LogInButton } from "../auth/LogInButton";
 
 export const NotAuthenticatedCard = () => {
   return (
-    <Card>
+    <Card className="m-auto mt-4 max-w-lg">
       <CardHeader>
         <CardTitle>You need to be logged in to view this page</CardTitle>
       </CardHeader>
+      <CardFooter>
+        <LogInButton />
+      </CardFooter>
     </Card>
   );
 };
